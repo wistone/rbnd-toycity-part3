6f4b1c8320e0dd8a54f2bf3eb32554b9eab64d76
Jianping Shi <shijianping5000@gmail.cm>
2016-02-21 18:19:04 +0800

    change indent

--- lib/product.rb
+++ lib/product.rb
@@ -1,47 +1,47 @@
 require_relative "errors"
 
 class Product
-	@@products = []
-	@@products_in_stock = []
+  @@products = []
+  @@products_in_stock = []
 
-	attr_reader :title, :price, :stock
-	def initialize(options={})
-		@@products.each do |product|
-			if product.title == options[:title]
-				raise DuplicateProductError, "'#{options[:title]}' already exists!"
-			end
-		end
-		@title = options[:title]
-		@price = options[:price]
-		@stock = options[:stock]
-		@@products << self
-		if (options[:stock] > 0)
-			@@products_in_stock << self
-		end
-	end
-	def in_stock?
-		@stock > 0
-	end
-	def print
- 		"title: " + @title + ", price: " + @price.to_s + ", stock: " + @stock.to_s
-	end
+  attr_reader :title, :price, :stock
+  def initialize(options={})
+    @@products.each do |product|
+      if product.title == options[:title]
+        raise DuplicateProductError, "'#{options[:title]}' already exists!"
+      end
+    end
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    @@products << self
+    if (options[:stock] > 0)
+      @@products_in_stock << self
+    end
+  end
+  def in_stock?
+    @stock > 0
+  end
+  def print
+    "title: " + @title + ", price: " + @price.to_s + ", stock: " + @stock.to_s
+  end
   def self.all
     @@products
   end
   def self.find_by_title(title)
-		@@products.each do |product|
-			if product.title == title
-				return product
-			end
-		end
-	end
-	def self.in_stock
-		@@products_in_stock
-	end
-	def conduct_transaction
-		@stock -= 1
-	end
-	def delete_transaction
-		@stock += 1
-	end
+    @@products.each do |product|
+      if product.title == title
+        return product
+      end
+    end
+  end
+  def self.in_stock
+    @@products_in_stock
+  end
+  def conduct_transaction
+    @stock -= 1
+  end
+  def delete_transaction
+    @stock += 1
+  end
 end
