dca09b3959f0fce582a2f7d59f90ccce2b8de048
Jianping Shi <shijianping5000@gmail.cm>
2016-02-21 17:01:36 +0800

    finish product part

--- lib/product.rb
+++ lib/product.rb
@@ -2,6 +2,7 @@ require_relative "errors"
 
 class Product
 	@@products = []
+	@@products_in_stock = []
 
 	attr_accessor :title, :price, :stock 
 	def initialize(options={})
@@ -14,8 +15,24 @@ class Product
 		@price = options[:price]
 		@stock = options[:stock]
 		@@products << self
+		if (options[:stock] > 0)
+			@@products_in_stock << self
+		end
+	end
+	def in_stock?
+		@stock > 0
 	end
   def self.all
     @@products
   end
+  def self.find_by_title(title)
+		@@products.each do |product|
+			if product.title == title
+				return product
+			end
+		end
+	end
+	def self.in_stock
+		@@products_in_stock
+	end
 end
